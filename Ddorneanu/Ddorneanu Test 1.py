41fe62022f79c2d394870356914dde1b87766f84
ddorneanu <ddorneanu@tremend.ro>
2016-07-19 12:24:52 +0300

    Initial commit

---
+++ Ddorneanu/Ddorneanu Test 1.py
@@ -0,0 +1,15 @@
+#dictionare
+nums = {
+    1: "one",
+    2: "two",
+    3: "three"
+}
+
+result = []
+print (1 in nums)
+print ("three" in nums)
+print (4 in nums)
+for index in nums:
+    if int(index)%2!=0:
+        result.append(nums[index])
+print ("numere prime: " + str(result))
